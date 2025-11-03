9478dd35b41c4895f0b5cf8ae30e18ed76129dd4
MartinMurithi <wachiramartin84@gmail.com>
2025-11-03 23:14:34 +0300

    auth service

---
+++ auth/cmd/server/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("[AUTH SERVICE ENABLED]: Ready to launch in 3 ..... 2 ...... 1.......\n")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/MartinMurithi/storeforge.io
+
+go 1.25.1
