008a8859a6fb403e0d2bb3c4fc87d72e1b8832a1
MartinMurithi <wachiramartin84@gmail.com>
2026-02-11 04:36:25 +0300

    fixed the 404 issue

--- usermanagement/internal/apperrors/errors.go
+++ usermanagement/internal/apperrors/errors.go
@@ -16,28 +16,30 @@ var (
 	ErrBusinessTypeRequired = errors.New("business type is required")
 	ErrBusinessNameRequired = errors.New("business name is required")
 	ErrIdIsRequired         = errors.New("id is required")
-	ErrRoleIsRequired         = errors.New("role is required")
+	ErrRoleIsRequired       = errors.New("role is required")
 	ErrInvalidInput         = errors.New("invalid input")
 )
 
 // Format/validation errors
 var (
-	ErrInvalidEmailFormat = errors.New("invalid email format")
-	ErrInvalidPhoneNumber = errors.New("invalid phone number")
-	ErrInvalidUUIDFormat  = errors.New("invalid UUID format")
+	ErrInvalidEmailFormat  = errors.New("invalid email format")
+	ErrInvalidPhoneNumber  = errors.New("invalid phone number")
+	ErrInvalidUUIDFormat   = errors.New("invalid UUID format")
 	ErrInvalidUserIdFormat = errors.New("invalid user id format")
 )
 
 // Business rule errors
 var (
 	ErrUserAlreadyExists         = errors.New("user already exists")
+	ErrUserEmailAlreadyExists    = errors.New("user with that email already exists")
+	ErrUserMobileExists          = errors.New("user with that mobile already exists")
 	ErrBusinessNameAlreadyExists = errors.New("business name already exists")
-	ErrAccountDeactivated = errors.New("your account has been deactivated. Please contact support.")
+	ErrAccountDeactivated        = errors.New("your account has been deactivated. Please contact support.")
 )
 
 // Credentials rule errors
 var (
-	ErrInvalidCredentials = errors.New("invalid email or password")
+	ErrInvalidCredentials = errors.New("invalid credentials")
 )
 
 // Database errors
